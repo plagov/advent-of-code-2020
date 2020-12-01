4184d6788548850e029a0c28dc7b06e7b507666c
Vitali Plagov <vitali.plagov@topia.com>
2020-12-01 12:04:34 +0200

    Break outer loop when condition is satisfied

--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -6,15 +6,15 @@ class Day01 {
 	private val allNumbers = File("src/test/resources/day01/expenses.txt").readLines()
 
 	@Test
-	fun test() {
-		for (i in allNumbers) {
+	fun findTwoNumbersThatSumTo2020() {
+		outerLoop@for (i in allNumbers) {
 			for (k in allNumbers.minus(i)) {
 				val first = i.toInt()
 				val second = k.toInt()
 				if (first + second == 2020) {
 					println("Numbers are $first and $second")
 					println("Multiply is: ${first * second}")
-					break
+					break@outerLoop
 				}
 			}
 		}
