73175efc90cf25b416566d88f66cacf466de34bc
Vitali Plagov <vitalii.plagov@gmail.com>
2020-12-03 22:38:09 +0200

    Solve part 2 of day 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,12 +1,12 @@
 class Day02 {
 
-  fun numberOfValidPasswordsFor(input: List<String>): Int {
+  fun numberOfValidPasswordsByRepetitionFor(input: List<String>): Int {
     var counter = 0
     input.forEach { entry ->
       val password = password(entry)
       val mandatoryChar = mandatoryChar(entry)
-      val minRepetition = minRepetition(entry)
-      val maxRepetition = maxRepetition(entry)
+      val minRepetition = firstPosition(entry)
+      val maxRepetition = secondPosition(entry)
 
       if (password.contains(mandatoryChar)) {
         val size = password.filter { it == mandatoryChar }.size
@@ -18,13 +18,26 @@ class Day02 {
     return counter
   }
 
+  fun numberOfValidPasswordsByPositionsFor(input: List<String>): Int {
+    return input.filter { entry ->
+      val password = password(entry)
+      val mandatoryChar = mandatoryChar(entry)
+      val firstPosition = firstPosition(entry) - 1
+      val secondPosition = secondPosition(entry) - 1
+
+      (password[firstPosition] == mandatoryChar || password[secondPosition] == mandatoryChar)
+        && !(password[firstPosition] == mandatoryChar && password[secondPosition] == mandatoryChar)
+    }.count()
+  }
+
   private fun password(entry: String) = entry.substringAfter(":").trim().toCharArray()
 
   private fun policy(entry: String) = entry.substringBefore(":").trim()
 
   private fun mandatoryChar(entry: String) = policy(entry).last()
 
-  private fun minRepetition(entry: String) = policy(entry).substringBefore("-").toInt()
+  private fun firstPosition(entry: String) = policy(entry).substringBefore("-").toInt()
 
-  private fun maxRepetition(entry: String) = policy(entry).substringAfter("-").substringBefore(" ").toInt()
+  private fun secondPosition(entry: String) = policy(entry).substringAfter("-")
+    .substringBefore(" ").toInt()
 }
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -10,10 +10,20 @@ private val day2 = Day02()
 class Day02Test : ShouldSpec({
 
   should("return correct number of valid passwords for sample input") {
-    day2.numberOfValidPasswordsFor(sampleInput) shouldBe 2
+    day2.numberOfValidPasswordsByRepetitionFor(sampleInput) shouldBe 2
   }
 
   should("return correct number for valid passwords for real input") {
-    day2.numberOfValidPasswordsFor(realInput) shouldBe 556
+    day2.numberOfValidPasswordsByRepetitionFor(realInput) shouldBe 556
   }
+
+  should("return correct number for valid passwords by positions for sample input") {
+    val sample = listOf("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc")
+    day2.numberOfValidPasswordsByPositionsFor(sample) shouldBe 1
+  }
+
+  should("return correct number for valid passwords by positions for real input") {
+    day2.numberOfValidPasswordsByPositionsFor(realInput) shouldBe 605
+  }
+
 })
