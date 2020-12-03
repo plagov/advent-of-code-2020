a52fae5200831472ef826d07042aac920eaee165
Vitali Plagov <vitalii.plagov@gmail.com>
2020-12-04 00:00:54 +0200

    Load files through `Class.getResource`

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,10 +1,10 @@
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
-import java.io.File
 
 class Day01Test : ShouldSpec({
 
-  val allNumbers = File("src/test/resources/day01/expenses.txt").readLines().map { it.toInt() }
+  val allNumbers = this::class.java.getResourceAsStream("inputs/day01.txt")
+    .bufferedReader().readLines().map { it.toInt() }
   val day1 = Day01()
 
   should("find two numbers that sum to 2020") {
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -1,13 +1,12 @@
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
-import java.io.File
-
-private val sampleInput = listOf("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc")
-private val realInput = File("src/test/resources/inputs/day02.txt").readLines()
-
-private val day2 = Day02()
 
 class Day02Test : ShouldSpec({
+
+  val sampleInput = listOf("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc")
+  val realInput = this::class.java.getResourceAsStream("inputs/day02.txt").bufferedReader().readLines()
+
+  val day2 = Day02()
 
   should("return correct number of valid passwords for sample input") {
     day2.numberOfValidPasswordsByRepetitionFor(sampleInput) shouldBe 2
