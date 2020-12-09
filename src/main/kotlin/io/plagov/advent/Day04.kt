ce8c24153236bc829f5fd1f11e0ff7ed56196abe
Vitalii Plagov <vitalii.plagov@gmail.com>
2020-12-09 22:48:55 +0200

    Parse input to passport entries

--- src/main/kotlin/io/plagov/advent/Day04.kt
+++ src/main/kotlin/io/plagov/advent/Day04.kt
@@ -2,8 +2,11 @@ package io.plagov.advent
 
 class Day04 {
   fun numberOfValidPasswords(input: List<String>): Int {
-    TODO("Not yet implemented")
+    val passports = parseInputToPassportEntries(input)
+    return 1
   }
 
+  private fun parseInputToPassportEntries(input: List<String>) =
+    input.joinToString().replace(",", "").split("  ")
 
 }
