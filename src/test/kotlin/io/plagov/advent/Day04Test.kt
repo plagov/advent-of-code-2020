52d71c3cdb6faef1e5719444e8279aac6fa8f992
Vitali Plagov <vitalii.plagov@gmail.com>
2020-12-11 00:33:49 +0200

    Solve part 2 - validate passport fields' values

--- src/test/kotlin/io/plagov/advent/Day04Test.kt
+++ src/test/kotlin/io/plagov/advent/Day04Test.kt
@@ -6,16 +6,25 @@ import io.plagov.readInputFile
 
 class Day04Test : ShouldSpec ({
 
-  val sampleInput = readInputFile("day04-sample.txt")
+  val part1SampleInput = readInputFile("day04-part1-sample.txt")
+  val part2SampleInput = readInputFile("day04-part2-sample.txt")
   val realInput = readInputFile("day04.txt")
   val day4 = Day04()
 
   should("return correct number of valid passports for sample input") {
-    day4.numberOfValidPasswords(sampleInput) shouldBe 2
+    day4.passportsWithValidFieldsOnly(part1SampleInput).size shouldBe 2
   }
 
   should("return correct number of valid passports for real input") {
-    day4.numberOfValidPasswords(realInput) shouldBe 206
+    day4.passportsWithValidFieldsOnly(realInput).size shouldBe 206
+  }
+
+  should("return correct number of valid passports with valid values for sample input") {
+    day4.passportsWithValidFieldsAndValues(part2SampleInput).size shouldBe 4
+  }
+
+  should("return correct number of valid passports with valid values for real input") {
+    day4.passportsWithValidFieldsAndValues(realInput).size shouldBe 123
   }
 
 })
