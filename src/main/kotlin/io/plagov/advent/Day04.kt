a15e180e171921bc31a0aff1bd99042204a212e5
Vitali Plagov <vitalii.plagov@gmail.com>
2020-12-10 22:37:52 +0200

    Finish core logic and solve first problem

--- src/main/kotlin/io/plagov/advent/Day04.kt
+++ src/main/kotlin/io/plagov/advent/Day04.kt
@@ -1,12 +1,23 @@
 package io.plagov.advent
 
 class Day04 {
-  fun numberOfValidPasswords(input: List<String>): Int {
-    val passports = parseInputToPassportEntries(input)
-    return 1
+
+  companion object {
+    private val MANDATORY_FIELDS = listOf("ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt",)
   }
+
+  fun numberOfValidPasswords(input: List<String>) =
+    parseInputToPassportEntries(input).filter { passport ->
+        MANDATORY_FIELDS.all { field -> passport.containsKey(field) }
+    }.size
 
   private fun parseInputToPassportEntries(input: List<String>) =
     input.joinToString().replace(",", "").split("  ")
+      .map { entry ->
+        entry.split(" ")
+          .map { field ->
+            field.substringBefore(":") to field.substringAfter(":")
+          }.toMap()
+      }
 
 }
--- src/test/kotlin/io/plagov/advent/Day04Test.kt
+++ src/test/kotlin/io/plagov/advent/Day04Test.kt
@@ -7,9 +7,15 @@ import io.plagov.readInputFile
 class Day04Test : ShouldSpec ({
 
   val sampleInput = readInputFile("day04-sample.txt")
+  val realInput = readInputFile("day04.txt")
   val day4 = Day04()
 
   should("return correct number of valid passports for sample input") {
     day4.numberOfValidPasswords(sampleInput) shouldBe 2
   }
+
+  should("return correct number of valid passports for real input") {
+    day4.numberOfValidPasswords(realInput) shouldBe 2
+  }
+
 })
