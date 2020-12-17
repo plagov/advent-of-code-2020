3004170383952404298605137b08a79d52136587
Vitalii Plagov <vitalii.plagov@gmail.com>
2020-12-17 23:50:31 +0200

    Add parameterized test to assert row number

--- src/test/kotlin/io/plagov/advent/Day05Test.kt
+++ src/test/kotlin/io/plagov/advent/Day05Test.kt
@@ -1,6 +1,10 @@
 package io.plagov.advent
 
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import io.kotest.matchers.shouldBe
 
 class Day05Test : ShouldSpec({
@@ -11,6 +15,17 @@ class Day05Test : ShouldSpec({
 
   should("return highest seat ID for sample boarding passes") {
     day5.highestSeatIdForPasses(samplePasses) shouldBe 820
+  }
+
+  should("return correct row number for sample boarding pass") {
+    forAll(
+      table(
+        headers("boarding pass", "row number"),
+        row("BFFFBBFRRR", 70),
+        row("FFFBBBFRRR", 14),
+        row("BBFFBBFRLL", 102)
+      )
+    ) { boardingPass, rowNumber -> day5.findRowNumberForPass(boardingPass) shouldBe rowNumber }
   }
 
 })
