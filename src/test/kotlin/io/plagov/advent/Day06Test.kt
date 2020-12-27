83ea6c670c921de0a11f64ba9df30ad065d00aff
Vitali Plagov <vitalii.plagov@gmail.com>
2020-12-27 23:40:44 +0200

    Add test for real input

--- src/test/kotlin/io/plagov/advent/Day06Test.kt
+++ src/test/kotlin/io/plagov/advent/Day06Test.kt
@@ -7,11 +7,17 @@ import io.plagov.readInputFile
 class Day06Test : ShouldSpec({
 
   val sampleInput = readInputFile("day06-sample.txt")
+  val realInput = readInputFile("day06.txt")
 
   val day6 = Day06()
 
   should("return correct sum of questions with answer 'yes' for sample input") {
     day6.sumForYesQuestions(sampleInput) shouldBe 11
   }
+
+  should("return correct sum of questions with answer 'yes' for real input") {
+    day6.sumForYesQuestions(realInput) shouldBe 6748
+  }
+
 
 })
