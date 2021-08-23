b44343eb3499462a3f8aaad9911e75929299e5a7
Vitali Plagov <vitalii.plagov@gmail.com>
2021-08-23 14:08:46 +0300

    Convert to direct return

--- src/test/kotlin/io/plagov/TestUtil.kt
+++ src/test/kotlin/io/plagov/TestUtil.kt
@@ -1,7 +1,6 @@
 package io.plagov
 
-fun readInputFile(fileName: String): List<String> {
-  return Thread.currentThread().contextClassLoader
+fun readInputFile(fileName: String) =
+  Thread.currentThread().contextClassLoader
     .getResourceAsStream("inputs/$fileName")?.bufferedReader()?.readLines()
     ?: throw IllegalStateException("Cannot find file inputs/$fileName under test resources")
-}
